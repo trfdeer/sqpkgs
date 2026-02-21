645998216c806db92856e3a7401d0d3726152766
Tuhin Tarafder <ttarafder7d1@protonmail.com>
2026-02-21 08:27:32 +0530

    feat(overlays): add Terraria server override and split overlay composition
    refactor(flake): split overlays into sq and terraria and compose as
    default
    refactor(devshell): reuse pkgs instead of re-importing nixpkgs

--- flake.nix
+++ flake.nix
@@ -15,12 +15,17 @@
       };
     in
     {
-      overlays.default = final: prev: {
-        sqpkgs = {
-          papermc = final.sqpkgs.papermcPackages.default;
-          papermcPackages = final.callPackage ./pkgs/games/minecraft/papermc { };
-          minecraftPlugins = final.callPackage ./pkgs/games/minecraft/plugins { };
+      overlays = {
+        sq = final: prev: {
+          sqpkgs = {
+            papermc = final.sqpkgs.papermcPackages.default;
+            papermcPackages = final.callPackage ./pkgs/games/minecraft/papermc { };
+            minecraftPlugins = final.callPackage ./pkgs/games/minecraft/plugins { };
+          };
         };
+        terraria = import ./pkgs/games/terraria;
+
+        default = final: prev: (self.overlays.sq final prev) // (self.overlays.terraria final prev);
       };
 
       packages.${system}.papermc = pkgs.sqpkgs.papermc;
@@ -30,18 +35,11 @@
         minecraftPlugins = pkgs.sqpkgs.minecraftPlugins;
       };
 
-      devShells.${system}.default =
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ self.overlays.default ];
-          };
-        in
-        pkgs.mkShellNoCC {
-          packages = with pkgs; [
-            nixd
-            nixfmt-rfc-style
-          ];
-        };
+      devShells.${system}.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          nixd
+          nixfmt-rfc-style
+        ];
+      };
     };
 }
